0b174a95c6085ad5a0809aa678483f2f06f5a342
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-09-21 00:06:27 +0300

    first interaction with react

--- src/index.js
+++ src/index.js
@@ -4,3 +4,41 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+function ResultItem() {
+    return (
+        <div>
+            <p>Bubby's</p>
+            <p>Price: $$$</p>
+            <p>Location: Tribeca</p>
+        </div>
+    );
+}
+
+export default ResultItem;
+
+import ResultItem from './ResultItem';
+
+function App() {
+    return (
+        <div>
+            <ResultItem />
+            <ResultItem />
+        </div>
+    );
+}
+
+
+//dynamic
+import ResultItem from './ResultItem';
+
+function App() {
+    return (
+        <div>
+            <ResultItem name="Bubby's" price="$$$" location="Tribeca"/>
+            <ResultItem name="Nathan's Famous" price="$" location="Coney Island"/>
+        </div>
+    );
+}
+
+import { messages, contacts } from "./data";
